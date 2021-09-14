4f3517f879260b109cca699d2f9f9b686bff4f05
joelmss93 <joelmss93@gmail.com>
2021-09-13 21:49:38 -0300

    refactor: pagination components

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,5 +1,37 @@
-export function PaginationItem() {
-  return (
+import { Button } from '@chakra-ui/react';
 
+interface PaginationItemProps {
+  pageNumber: number;
+  isCurrent?: boolean;
+}
+
+export function PaginationItem({
+  isCurrent = false,
+  pageNumber,
+}: PaginationItemProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        size="sm"
+        fontSize="sm"
+        w="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{ bgColor: 'pink.500', cursor: 'not-allowed' }}
+      >
+        {pageNumber}
+      </Button>
+    );
+  }
+  return (
+    <Button
+      size="sm"
+      fontSize="xs"
+      w="4"
+      bg="gray.700"
+      _hover={{ bg: 'gray.500' }}
+    >
+      {pageNumber}
+    </Button>
   );
 }
