61083518e8cc494946a6a422bfcddd18f9efca03
joelmss93 <joelmss93@gmail.com>
2021-09-15 22:11:02 -0300

    feat: navigation

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -15,16 +15,16 @@ export function SidebarNav() {
         <NavLink icon={RiDashboardLine} href="/dashboard">
           Dashboard
         </NavLink>
-        <NavLink icon={RiContactsLine} href="/dashboard">
+        <NavLink icon={RiContactsLine} href="/users">
           Usuários
         </NavLink>
       </NavSection>
 
       <NavSection title="AUTOMAÇÃO">
-        <NavLink icon={RiInputMethodLine} href="/dashboard">
+        <NavLink icon={RiInputMethodLine} href="/forms">
           Formulários
         </NavLink>
-        <NavLink icon={RiGitMergeLine} href="/dashboard">
+        <NavLink icon={RiGitMergeLine} href="/automation">
           Automação
         </NavLink>
       </NavSection>
