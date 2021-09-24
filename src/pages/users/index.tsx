f99a894f106e4aa0fe50e4779352c688e5884fc7
joelmss93 <joelmss93@gmail.com>
2021-09-24 18:50:13 -0300

    chore: prefetch data and cache invalidate

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,17 +14,20 @@ import {
   Td,
   Text,
   Spinner,
+  Link,
   useBreakpointValue,
 } from '@chakra-ui/react';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
 
 import { useUsers } from '../../services/hooks/useUsers';
+import { queryClient } from '../../services/queryClient';
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -34,6 +37,20 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ['user', userId],
+      async () => {
+        const { data } = await api.get(`users/${userId}`);
+
+        return data;
+      },
+      {
+        staleTime: 1000 * 60 * 10, // 10 minutes fresh
+      },
+    );
+  }
 
   return (
     <Box>
@@ -50,7 +67,7 @@ export default function UserList() {
                 <Spinner size="sm" color="gray.500" ml="4" />
               )}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -60,7 +77,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -90,7 +107,13 @@ export default function UserList() {
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{user.name}</Text>
+                          <Link
+                            href="/"
+                            color="purple.400"
+                            onMouseEnter={() => handlePrefetchUser(user.id)}
+                          >
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </Link>
                           <Text fontSize="small" color="gray.300">
                             {user.email}
                           </Text>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { createServer, Factory, Model, Response } from 'miragejs';
+import {
+  createServer,
+  Factory,
+  Model,
+  Response,
+  ActiveModelSerializer,
+} from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -10,6 +16,10 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+
     models: {
       user: Model.extend<Partial<User>>({}),
     },
@@ -44,13 +54,14 @@ export function makeServer() {
         const pageStart = (Number(page) - 1) * Number(per_page);
         const pageEnd = pageStart + Number(per_page);
 
-        const users = this.serialize(schema.all('user')).users.slice(
-          pageStart,
-          pageEnd,
-        );
+        const users = this.serialize(schema.all('user'))
+          .users.sort((a, b) => a.createdAt - b.createdAt)
+          .slice(pageStart, pageEnd);
 
         return new Response(200, { 'x-total-count': String(total) }, { users });
       });
+
+      this.get('/users/:id');
       this.post('/users');
 
       this.namespace = '';
